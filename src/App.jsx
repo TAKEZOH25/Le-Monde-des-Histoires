7056b1ff439dc1e8c576e7d68f6760e4cf0995c3
TAKEZOH25 <flocquet.emmanuel@gmail.com>
2025-12-19 22:23:55 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-red-500">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
